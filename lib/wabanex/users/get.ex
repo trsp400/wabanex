0a9d42745a33380ffa3955d2523fbfd1e9cd8606
Thiago Robles <roblesthiago38@gmail.com>
2021-06-21 23:58:22 -0300

    feat: create read function to read users by ID

---
+++ lib/wabanex/users/get.ex
@@ -0,0 +1,21 @@
+defmodule Wabanex.Users.Get do
+  alias Wabanex.{Repo, User}
+  alias Ecto.UUID
+
+  def call(id) do
+    id
+    |> UUID.cast()
+    |> handle_response()
+  end
+
+  defp handle_response(:error) do
+    {:error, "Invalid UUID"}
+  end
+
+  defp handle_response({:ok, uuid}) do
+    case Repo.get(User, uuid) do
+      nil -> {:error, "User not found"}
+      user -> {:ok, user}
+    end
+  end
+end
