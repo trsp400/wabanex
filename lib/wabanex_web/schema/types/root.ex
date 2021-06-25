c7f67bd64ea02cb0adce1f7e69d8c89586e09420
Thiago Robles <roblesthiago38@gmail.com>
2021-06-24 22:55:05 -0300

    feat: createUser mutation and start creating getUsers to query all users from database

--- lib/wabanex_web/schema/types/root.ex
+++ lib/wabanex_web/schema/types/root.ex
@@ -11,5 +11,17 @@ defmodule WabanexWeb.Schema.Types.Root do
 
       resolve &UserResolver.get/2
     end
+
+    field :get_users, type: :user do
+      resolve &UserResolver.getAll/2
+    end
+  end
+
+  object :root_mutation do
+    field :create_user, type: :user do
+      arg :input, non_null(:create_user_input)
+
+      resolve &UserResolver.create/2
+    end
   end
 end
